6bdd3141cb888dc8c3c79da8a60a948c6ddbdfd2
AJ <amarjitbhandal@gmail.com>
2023-03-21 23:32:58 +1100

    Add support for json-server and fetch meals from the server.

--- src/components/Meals/AvailableMeals.tsx
+++ src/components/Meals/AvailableMeals.tsx
@@ -1,43 +1,55 @@
 // @ts-ignore
 import classes from './AvailableMeals.module.css';
 import Card from "../UI/Card";
-import MealItem from "./MealItem/MealItem";
-
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
-
+import MealItem, {MealItemProps} from "./MealItem/MealItem";
+import {useEffect, useState} from "react";
+import axios, {AxiosError, CanceledError} from "axios";
 
 interface AvailableMealsProps {
-
 }
+
+const baseUrl = 'http://localhost:3500/meals';
 
 const AvailableMeals = ({}: AvailableMealsProps): JSX.Element => {
 
-    const mealsList = DUMMY_MEALS.map(meal => {
+    const [meals, setMeals] = useState<MealItemProps[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<AxiosError | string | null | undefined>(null);
+
+    useEffect(() => {
+        const abortController = new AbortController();
+
+        setIsLoading(true);
+
+        axios.get(baseUrl, {
+            headers: {
+                'Accept': 'application/json'
+            },
+            signal: abortController.signal
+        })
+             .then(response => {
+                 setMeals(response.data as MealItemProps[]);
+                 setIsLoading(false);
+             })
+             .catch(e => {
+                 setIsLoading(false);
+
+                 if (e instanceof CanceledError) {
+                     return;
+                 } else if (e instanceof AxiosError) {
+                     setError(e.code ? e.code : e);
+                 } else {
+                     setError(e);
+                     throw Error(e);
+                 }
+             });
+
+        return () => {
+            abortController.abort('Cancelled by abort abortController');
+        }
+    }, []);
+
+    const mealsList = meals.map(meal => {
         return (
             <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price}/>
         )
@@ -46,10 +58,14 @@ const AvailableMeals = ({}: AvailableMealsProps): JSX.Element => {
     return <section className={classes.meals}>
         <Card>
             <ul>
-                {mealsList}
+                {isLoading
+                    ? <p>Data is being fetched</p>
+                    :  error !== null
+                        ? <p style={{color: 'darkred'}}>{error!.toString()}</p>
+                        : mealsList}
             </ul>
         </Card>
-    </section>
+    </section>;
 }
 
 export default AvailableMeals;
